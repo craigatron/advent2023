5008c34dcae47e1d07eb38af8a51f947e2cea157
Craig Martek <craigmartek@gmail.com>
2023-12-09 11:40:45 -0500

    day8 (#7)

---
+++ template.rs
@@ -0,0 +1,37 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::time::Instant;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let file = &args[1];
+    let start1 = Instant::now();
+    let p1 = part1(file);
+    let duration1 = start1.elapsed();
+    println!("part1: {}, time {:?}", p1, duration1);
+
+    let start2 = Instant::now();
+    let p2 = part2(file);
+    let duration2 = start2.elapsed();
+    println!("part2: {}, time {:?}", p2, duration2);
+}
+
+fn part1(file: &str) -> u32 {
+    0
+}
+
+fn part2(file: &str) -> u32 {
+    0
+}
+
+// borrowed from https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
